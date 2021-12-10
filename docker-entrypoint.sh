ec4f6993b0ea13620cb6d3a87b30f2135a086c47
mARTin <91568406+mARTin-B78@users.noreply.github.com>
2021-12-10 12:56:38 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 BRANCH=${BRANCH:-"develop"} 
 GIT_UPSTREAM=${GIT_UPSTREAM:-"https://github.com/Superalgos/Superalgos.git"}
 GIT_REPOSITORY_URL=${GIT_REPOSITORY_URL:-$GIT_UPSTREAM}
-GIT_USERNAME=${GIT_USERNAME:-""}
+GIT_USERNAME=${GIT_USERNAME:-"superalgos"}
 GIT_PERSONAL_ACCESS_TOKEN=${GIT_PERSONAL_ACCESS_TOKEN:=""}
 GIT_EMAIL_ADDRESS=${GIT_EMAIL_ADDRESS:=""}
 
@@ -27,29 +27,29 @@ if [ -d "/app/Superalgos/.git" ]; then
         git pull
     fi
 else
-    # clone repo
+    # clone Superalgos repo
 	git init
     git clone "${GIT_REPOSITORY_URL}" /app/Superalgos
     if [ "${GIT_REPOSITORY_URL}" != "${GIT_UPSTREAM}" ]; then
         git remote add upstream "${GIT_UPSTREAM}"
     fi
+	cd /app/Superalgos
+
+    #
+    git config --global user.email "${GIT_EMAIL_ADDRESS}"                                                       
+    git config --global user.name "${GIT_USERNAME}"  
+    git config --get remote.origin.url
+
+    # ensure node dependencies are up to date # caused an error when restarting Superalgos
+    node setup noShortcuts
+	
+	cd ..
+    cd ..
+    addgroup superalgos
+    adduser --disabled-password --no-create-home --ingroup superalgos superalgos 
+  # chown -R superalgos:superalgos /app   # Did not work with rasbery pi
+    chown -R 1000:1000 /app 
 fi
-
-cd /app/Superalgos
-
-#
-git config --global user.email "${GIT_EMAIL_ADDRESS}"                                                       
-git config --global user.name "${GIT_USERNAME}"  
-git config --get remote.origin.url
-
-# ensure node dependencies are up to date
-node setup noShortcuts
-
-cd ..
-cd ..
-addgroup superalgos
-adduser --disabled-password --no-create-home --ingroup superalgos superalgos 
-chown -R superalgos:superalgos /app 
 
 # run the application
 cd /app/Superalgos
