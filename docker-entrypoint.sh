0566b34aa204a1c182141210864a67ae72e071f8
mARTin <91568406+mARTin-B78@users.noreply.github.com>
2021-12-11 03:51:20 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Stops the Script if ANY Error occures
 set -eo pipefail
 
 # env vars
@@ -19,43 +20,67 @@ fi
 # check if code already exists
 if [ -d "/app/Superalgos/.git" ]; then
     # update code
+	echo UPDATE CORE
     cd /app/Superalgos
     git checkout ${BRANCH}
     if git remote | grep upstream ; then
         if git pull upstream develop ; then
-           echo "updated-1"
+           echo "UPDATED DEVELOP BRANCH"
         else
-           echo "already up to date, or other error occurred"
+           echo "ALREADY UP TO DATE, OR ERROR OCCURED"
         fi
     else
         git pull
-		echo "updated-2"
+		echo "UPDATED MASTER BRANCH ???"
     fi
 else
     # clone Superalgos repo
+	echo INSTALL SUPERALGOS
 	git init
     git clone "${GIT_REPOSITORY_URL}" /app/Superalgos
     if [ "${GIT_REPOSITORY_URL}" != "${GIT_UPSTREAM}" ]; then
         git remote add upstream "${GIT_UPSTREAM}"
+		git fetch upstream
+		git checkout ${BRANCH}
     fi
 	cd /app/Superalgos
 
     #
+	echo "SET GIT EMAIL AND USERNAME AND REMOTE URL"
     git config --global user.email "${GIT_EMAIL_ADDRESS}"                                                       
     git config --global user.name "${GIT_USERNAME}"  
     git config --get remote.origin.url
 
-    # ensure node dependencies are up to date # caused an error when restarting Superalgos
-    node setup noShortcuts
 
-    cd ..
+    # ensure node dependencies are up to date # caused an error when restarting Superalgos
+	echo RUNNING NODE SETUP
+    node setup noShortcuts
+	
+	echo SETTING USER AND GROUP
+	cd ..
     cd ..
     addgroup superalgos
     adduser --disabled-password --no-create-home --ingroup superalgos superalgos 
   # chown -R superalgos:superalgos /app   # Did not work with rasbery pi
     chown -R 1000:1000 /app 
+	echo SUPERALGOS INSTALLATION FINISHED
 fi
+
 
 # run the application
 cd /app/Superalgos
+echo MAKEING SURE YOUR FORK IS IN SYNC WITH SUPERALGOS
+
+if git remote add upstream ${GIT_UPSTREAM} ; then
+   echo SETTING REMOTE UPSTREAM to ${GIT_UPSTREAM}
+   git remote add upstream ${GIT_UPSTREAM}
+else
+   echo REMOTE UPSTREAM ALREADY EXISTS AND IS SET TO ${GIT_UPSTREAM}
+   echo OR ERROR OCCURED
+fi
+git fetch upstream
+git checkout ${BRANCH}
+git rebase upstream/${BRANCH}
+
+echo STARTING SUPERALGOS
 node platform minMemo noBrowser
