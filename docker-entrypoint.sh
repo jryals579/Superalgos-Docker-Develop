48230969aeb7e1121426d41f5895f3458eab3448
mARTin <91568406+mARTin-B78@users.noreply.github.com>
2021-12-11 14:30:19 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,77 +10,80 @@ GIT_USERNAME=${GIT_USERNAME:-"superalgos"}
 GIT_PERSONAL_ACCESS_TOKEN=${GIT_PERSONAL_ACCESS_TOKEN:=""}
 GIT_EMAIL_ADDRESS=${GIT_EMAIL_ADDRESS:=""}
 
+echo "XXXXXXXXXX SET GITHUB USERNAME AND PERSONAL ACCESS TOKEN XXXXXXXXXX"
+echo "XXXXXXXXXX GIT USERNAME = ${GIT_USERNAME} XXXXXXXXXX"
 if [ "${GIT_USERNAME}" != "" ]; then
     GIT_REPOSITORY_URL="https://github.com/${GIT_USERNAME}/Superalgos.git"
+	echo XXXXXXXXXX GIT_REPOSITORY_URL = "https://github.com/${GIT_USERNAME}/Superalgos.git" XXXXXXXXXX
 fi
 if [ "${GIT_PERSONAL_ACCESS_TOKEN}" != "" ]; then
     GIT_REPOSITORY_URL="https://strafe:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/${GIT_USERNAME}/Superalgos.git"
+	echo XXXXXXXXXX GIT_REPOSITORY_URL = "https://strafe:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/${GIT_USERNAME}/Superalgos.git" XXXXXXXXXX
 fi
 
 # check if code already exists
+echo XXXXXXXXXX CHECK IF SUPERALGOS IS ALREADY INSTALLED XXXXXXXXXX
 if [ -d "/app/Superalgos/.git" ]; then
     # update code
-	echo UPDATE CORE
+	echo XXXXXXXXXX SUPERALGOS IS INSTALLED XXXXXXXXXX
     cd /app/Superalgos
     git checkout ${BRANCH}
+	echo XXXXXXXXXX CURRENT BRANCH IS ${BRANCH} XXXXXXXXXX
     if git remote | grep upstream ; then
         if git pull upstream develop ; then
-           echo "UPDATED DEVELOP BRANCH"
+           echo "XXXXXXXXXX ALREADY UP TO DATE XXXXXXXXXX"
+		   echo  XXXXXXXXXX CURRENT BRANCH IS ${BRANCH} XXXXXXXXXX
         else
-           echo "ALREADY UP TO DATE, OR ERROR OCCURED"
+		   echo "XXXXXXXXXX UPDATED DEVELOP BRANCH XXXXXXXXXX"
+		   git fetch upstream
+		   git rebase upstream/${BRANCH}
         fi
     else
         git pull
-		echo "UPDATED MASTER BRANCH ???"
+		echo "XXXXXXXXXX NOT ON DEVELOPER BRANCH ??? XXXXXXXXXX"
     fi
 else
     # clone Superalgos repo
-	echo INSTALL SUPERALGOS
+	echo XXXXXXXXXX NOT INSTALLED YET - INSTALLING SUPERALGOS XXXXXXXXXX
+	echo XXXXXXXXXX WORKDIR IS $PWD XXXXXXXXXX
 	git init
     git clone "${GIT_REPOSITORY_URL}" /app/Superalgos
-    if [ "${GIT_REPOSITORY_URL}" != "${GIT_UPSTREAM}" ]; then
+    if [ "${GIT_REPOSITORY_URL}" != "${GIT_UPSTREAM}" ]; 
+	  then
+	    cd Superalgos
         git remote add upstream "${GIT_UPSTREAM}"
 		git fetch upstream
 		git checkout ${BRANCH}
+	  else
+       echo ERROR	  
     fi
-	cd /app/Superalgos
 
     #
-	echo "SET GIT EMAIL AND USERNAME AND REMOTE URL"
+	echo XXXXXXXXXX SET GIT EMAIL AND USERNAME AND REMOTE URL XXXXXXXXXX
     git config --global user.email "${GIT_EMAIL_ADDRESS}"                                                       
     git config --global user.name "${GIT_USERNAME}"  
     git config --get remote.origin.url
-
+    echo XXXXXXXXXX GIT USERNAME = ${GIT_USERNAME} XXXXXXXXXX
+	echo XXXXXXXXXX GIT EMAIL = ${GIT_EMAIL_ADDRESS} XXXXXXXXXX
+	
 
     # ensure node dependencies are up to date # caused an error when restarting Superalgos
-	echo RUNNING NODE SETUP
+	echo XXXXXXXXXX RUNNING NODE SETUP XXXXXXXXXX
+	echo XXXXXXXXXX PLEASE BE PATIENT - IT CAN TAKE 5 TO 30 MINUTES XXXXXXXXXX
     node setup noShortcuts
 	
-	echo SETTING USER AND GROUP
+	echo XXXXXXXXXX SETTING USER AND GROUP XXXXXXXXXX
 	cd ..
     cd ..
     addgroup superalgos
     adduser --disabled-password --no-create-home --ingroup superalgos superalgos 
   # chown -R superalgos:superalgos /app   # Did not work with rasbery pi
     chown -R 1000:1000 /app 
-	echo SUPERALGOS INSTALLATION FINISHED
+	echo XXXXXXXXXX SUPERALGOS INSTALLATION FINISHED XXXXXXXXXX 
 fi
 
 
 # run the application
 cd /app/Superalgos
-echo MAKEING SURE YOUR FORK IS IN SYNC WITH SUPERALGOS
-
-if git remote add upstream ${GIT_UPSTREAM} ; then
-   echo SETTING REMOTE UPSTREAM to ${GIT_UPSTREAM}
-   git remote add upstream ${GIT_UPSTREAM}
-else
-   echo REMOTE UPSTREAM ALREADY EXISTS AND IS SET TO ${GIT_UPSTREAM}
-   echo OR ERROR OCCURED
-fi
-git fetch upstream
-git checkout ${BRANCH}
-git rebase upstream/${BRANCH}
-
-echo STARTING SUPERALGOS
+echo XXXXXXXXXX STARTING SUPERALGOS XXXXXXXXXX
 node platform minMemo noBrowser
